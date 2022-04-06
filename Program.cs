0c7ef509e116e80a98f1297d4ba5de6658afb6bd
Arthurmegon <m.arthur@aluno.edu.br>
2022-04-06 11:35:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Quadrado q1 = new Quadrado(5.0);
+
+Console.WriteLine(q1.Lado);
+
+q1.Lado = 2.0;
+
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+Retangulo r1 = new Retangulo(8.0, 3.0);
+
+Console.WriteLine(r1.Base);
+
+r1.Base = 3.0;
+
+Console.WriteLine(r1.Altura);
+Console.WriteLine(r1.Area);
+Console.WriteLine(r1.Perimetro);
